6ce8d473c7bd4e607547825337b9ee43c70386d3
Mira <gunzm1@bfh.ch>
2014-11-16 11:27:45 +0100

    modul testmässig eingebaut :)

--- Solution/frontend/app/main.js
+++ Solution/frontend/app/main.js
@@ -1,13 +1,20 @@
 define(
-	["backbone", "stardog","underscore","text!app/templates/main.html", "text!app/templates/elements.html"],
-	function(Backbone, Stardog,_, MainView, ElementsView) {
+	["backbone", 
+	 "stardog",
+	 "underscore",
+	 "test",
+	 "text!app/templates/main.html",
+	 "text!app/templates/elements.html"],
+	function(Backbone, Stardog,_,Test, MainView, ElementsView) {
 	var App = Backbone.View.extend({
 		el: $('#main'),
 		events: {
 			'click #buttonid': 'query'
 		},
 
-		initialize: function(){			
+		initialize: function(){	
+			
+			this.addtest();
 			console.log("it's working!");
 			
 			this.conn = new Stardog.Connection();
@@ -15,6 +22,17 @@ define(
 			this.conn.setCredentials("admin", "admin");
 
 			this.render();
+		},
+		addtest: function(){
+			console.log("jezt bin ich in der Addtest Function");
+			var initialize = function(){
+				Test.initialize();
+			}			
+			return {
+				initialize: initialize 
+			};
+			console.log(initialize);
+			//$(this.$el).html(initialize);			
 		},
 		render: function(){
 
