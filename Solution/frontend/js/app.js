ed832755ff897972dda23b6c4c2cdb01632f5f02
Sven Osterwalder <sven.osterwalder@gmail.com>
2014-11-23 00:32:31 +0100

    Re-structuring of project

--- Solution/frontend/js/app.js
+++ Solution/frontend/js/app.js
@@ -1,81 +1,117 @@
-App = Ember.Application.create({});
-var posts = [{
-  id: '1',
-  title: "Rails is Omakase",
-  author: { name: "d2h" },
-  date: new Date('12-27-2012'),
-  excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
-  body: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
-}, {
-  id: '2',
-  title: "The Parley Letter",
-  author: { name: "d2h" },
-  date: new Date('12-24-2012'),
-  excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
-  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."  
-}];
+(function(root) {
+    require(['config'], function(config) {
+        requirejs.config(config);
 
-App.Router.map(function() {
-  this.resource('step2');
-  this.resource('step1', function() {
-  });
-});
+        require(['ember', 'stardog'], function(Ember, stardog) {
+            function performQuery() {
+              return new Ember.RSVP.Promise(function(resolve, reject) {
+                var connection = new Stardog.Connection();
+                connection.setEndpoint(config.sparql.endpoint);
+                connection.setCredentials(
+                  config.sparql.user,
+                  config.sparql.password
+                );
+                console.log("trying to connect..", connection);
+                connection.query({
+                  database: config.sparql.database,
+                  query: '\
+                    select distinct\
+                      (strafter(str(?s), "#") AS ?haaas)\
+                    where\
+                      { ?s ?p ?o }\
+                  ',
+                  limit: 10,
+                  offset: 0
+                }, function(data) {
+                  resolve(data);
+                });
+              });
+            }
 
-App.Step1Controller = Ember.ArrayController.extend({
-    actions : {
-        goToStep2 : function() {
-            console.log('step1 trallalaaa');
-			this.transitionToRoute('step2'); 
-			this.createConnection()
-        },		
-    },
-	
-	createConnection: function(){
-	   this.conn = new Stardog.Connection();
-	   this.conn.setEndpoint("http://elephantsearch.bfh.ch:5820/");
-	   this.conn.setCredentials("admin", "admin");	
-		console.log('createConnection function aus action aufgerufen');
-		//this.query();
-	}//,
-	// query: function() {
-		// this.conn.query({
-			// database: "reiseplaner",
-			// query: '\
-				// select distinct\
-					// (strafter(str(?s), "#") AS ?haaas)\
-				// where\
-					// { ?s ?p ?o }\
-			// ',
-			// limit: 10,
-			// offset: 0
-		// },this.setResult.bind(this));
-	// },
-	// setResult: function(data){
-		// step1Result = data.results.bindings;
-	// }
-});
-	
-App.Step2Controller = Ember.ArrayController.extend({
-    actions : {
-        logtest : function() {
-            console.log('tralalaaaa');
-        }	
-    }
-	
-});
-App.Step2Route = Ember.Route.extend({
-  model: function() {
-  return posts;
-    //return step1Result;
-  }
-});
+            var appName = config.appName || "App";
 
-var showdown = new Showdown.converter();
+            App = Ember.Application.create();
 
-Ember.Handlebars.helper('format-markdown', function(input) {
-  return new Handlebars.SafeString(showdown.makeHtml(input));
-});
+            App.Router.map(function() {
+              this.route('welcome');
+              this.route('about');
+              this.route('contact');
+              this.route('step1');
+              this.route('step2');
+              this.route('step3');
+            });
 
-Ember.Handlebars.helper('format-date', function(date) {
-  return moment(date).fromNow();
-});
+            App.IndexRoute = Ember.Route.extend({
+              beforeModel: function() {
+                this.transitionTo('welcome');
+              }
+            });
+
+            App.WelcomeRoute = Ember.Route.extend({
+              afterModel: function() {
+                $(document).attr('title', 'Traveling OWL - Welcome!');
+              },
+              actions: {
+                step1: function(id) {
+                  console.log(id);
+                  this.transitionTo('step1');
+                }
+              }
+            });
+
+            App.AboutRoute = Ember.Route.extend({
+              afterModel: function() {
+                $(document).attr('title', 'Traveling OWL - About');
+              }
+            });
+
+            App.Step1Route = Ember.Route.extend({
+              model: function () {
+                return performQuery().then(function(data) {
+                  console.log("Query result ", data);
+                  return data.results.bindings;
+
+                });
+              },
+              actions: {
+                step2: function() {
+                  this.transitionTo('step2');
+                }
+              }
+            });
+
+            App.Step2Route = Ember.Route.extend({
+              model: function () {
+                return performQuery().then(function(data) {
+                  console.log("Query result ", data);
+                  return data.results.bindings;
+
+                });
+              },
+              actions: {
+                step1: function() {
+                  this.transitionTo('step1');
+                },
+                step3: function() {
+                  this.transitionTo('step3');
+                }
+              }
+            });
+
+            App.Step3Route = Ember.Route.extend({
+              model: function () {
+                return performQuery().then(function(data) {
+                  console.log("Query result ", data);
+                  return data.results.bindings;
+
+                });
+              },
+              actions: {
+                step2: function() {
+                  this.transitionTo('step2');
+                }
+              }
+            });
+        });
+    });
+})(this);
