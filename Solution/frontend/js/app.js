e82be4f4c357c43eb7289db891a0ad16d22f0c87
Mira <gunzm1@bfh.ch>
2014-12-03 17:46:52 +0100

    benutzeroberfläche

--- Solution/frontend/js/app.js
+++ Solution/frontend/js/app.js
@@ -3,7 +3,7 @@
         requirejs.config(config);
 		
         require(['ember', 'stardog','ember-data'], function(Ember, stardog,DS) {
-            function performQuery(queryString) {
+            function performQuery(modelName, queryString) {
               return new Ember.RSVP.Promise(function(resolve, reject) {
                 var connection = new Stardog.Connection();
                 connection.setEndpoint(config.sparql.endpoint);
@@ -11,14 +11,23 @@
                   config.sparql.user,
                   config.sparql.password
                 );
-                console.log("trying to connect..", connection);
+				connection.setReasoning("SL");
+                console.log("Performing query: ", queryString.trim());
                 connection.query({
                   database: config.sparql.database,
                   query: queryString,
                   limit: 10,
                   offset: 0
                 }, function(data) {
-                  resolve(data);
+					if(! data) {
+						reject("Fehler");
+					} else {
+						var result = {
+							modelName: modelName,
+							data: data
+						};
+						resolve(result);
+					}        
                 });
               });
             }
@@ -28,14 +37,14 @@
             App = Ember.Application.create();
 			App.ApplicationStore = DS.Store.extend();
 			App.ReiseModel = DS.Model.extend({
-				routeName: DS.attr('string')
+				routeName: DS.attr('string'),
 				//propertyList: DS.hasMany('App.PropertyModel')	
 			});
-			App.PropertyModel = DS.Model.extend({
-				propertyName: DS.attr('string')	
-			});
+			//App.PropertyModel = DS.Model.extend({
+			//	propertyName: DS.attr('string')	
+			//});
 			App.routeList = [];
-			
+		
             App.Router.map(function() {
               this.route('welcome');
               this.route('about');
@@ -48,14 +57,13 @@
             });
 
             App.IndexRoute = Ember.Route.extend({
-              beforeModel: function() {
-                this.transitionTo('welcome');
+              beforeModel: function() {	  
+                this.transitionTo('welcome');				
               }
             });
 			
 			App.ReiseModelsRoute = Ember.Route.extend({
 				model: function() {
-					//return this.store.find('reiseModel');
 					console.log("Hallo..", this.store);
 					var reisen = this.store.all('reiseModel');
 					console.log("Reisen: ", reisen);
@@ -72,6 +80,9 @@
 			});
 			
             App.WelcomeRoute = Ember.Route.extend({
+			beforeModel: function(){
+				//this.store.unloadAll("reiseModel");					
+			},
               afterModel: function() {
                 $(document).attr('title', 'Traveling OWL - Welcome!');
               },
@@ -90,6 +101,9 @@
             });
 			
             App.Step1Route = Ember.Route.extend({
+				beforeModel: function() {
+					this.store.unloadAll("reiseModel");
+				},
               model: function () {
 				var queryString = '\
                     select distinct\
@@ -97,42 +111,37 @@
                     where\
                       { ?s :reise true }\
                   ';
-                return performQuery(queryString).then(function(data) {
-                  console.log("step1 Query result ", data);
-                  return data.results.bindings;
+                return performQuery(null, queryString).then(function(result) {
+                  console.log("step1 Query result ", result);
+                  return result.data.results.bindings;
 
-                });
+                }).catch(function(data){
+					// TODO Fehlerhandling
+					console.log("catch: ", data);
+				});
               },
 			  fillModelList : function(){
 				console.log(this.store);
 				console.log("routeElements: "+ document.querySelectorAll(".routeElements"));
 				var routeElements = document.querySelectorAll(".routeElements");
 				nodes = Array.prototype.slice.call(routeElements,0);
-				nodes.forEach(function(r) {
+				nodes.forEach((function(r) {
 					if (r.checked) {
 						console.log("checked: " + r.name);
-						// TODO: das funktioniert hier nicht!
-						//this.store.createRecord('reiseModel',{
-						//	routeName: r.name
-						// });					
+						var name = r.name;
+						var tempModel = this.store.createRecord('reiseModel',{
+							routeName: name
+						 });
+						//App.routeList.push(tempModel);
 					} else{
 						console.log("not checked: " + r.name);
 					}
-				});
-				this.store.createRecord('reiseModel',{
-					routeName: 'Ausflug'
-				});			
-				// alle Models in liste füllen
+				}).bind(this));						
 				var temp = this.store.all('reiseModel');
-				temp.forEach(function(r) {
-					console.log("Temp: ", r);
-					App.routeList.push(r);
-				});
-				
-				console.log("Listenelement: " + App.routeList[0].get('routeName'));
+				console.log("das ist unser temp: ", temp);
 			  },			  
               actions: {
-                step2: function() {	
+                step2: function() {
 				  this.fillModelList();
                   this.transitionTo('step2');
                 }
@@ -140,27 +149,70 @@
 			  
             });
             App.Step2Route = Ember.Route.extend({
-				// TODO: sehr fiies, überlegen wie man das query zusammenbaut wenns mehreres häckchen hat
-				// zuerst Ausflug, dann Restaurant in einzelnem step?
-				// oder untereinander?
               model: function () {
-				var queryString = '\
-                    select distinct\
-                      (strafter(str(?s), "#") AS ?property)\
-                    where\
-                      { ?s rdfs:domain :' + App.routeList[0].get('routeName') + '  }';
-                return performQuery(queryString).then(function(data) {
-                  console.log("Query result ", data);
-                  return data.results.bindings;
-
-                });
+				var querys =[];
+				this.store.all('reiseModel').get('content').forEach(function(reiseModel) {
+					var queryString = '\
+						select distinct\
+						  (strafter(str(?s), "#") AS ?property)\
+						where\
+						  { ?s rdfs:domain :' + reiseModel.get('routeName') + '  }';
+					querys.push(performQuery(reiseModel.get('routeName'), queryString));					  
+					  if (reiseModel.get('routeName') == 'Ausflug'){
+						console.log('if abfrage funktioniert');
+						
+						queryString = '\
+							select distinct\
+							  (strafter(str(?j), "#") AS ?property)\
+							where\
+							  { ?j  rdfs:subClassOf :Jahreszeit   }';
+						querys.push(performQuery('Jahreszeit abhängig?:', queryString));
+						 
+						queryString = '\
+							select distinct\
+							  (strafter(str(?r), "#") AS ?property)\
+							where\
+							  { ?r  rdf:type :Region}';
+						querys.push(performQuery('Wähle eine Region aus', queryString));						
+					  };
+					  if (reiseModel.get('routeName') == 'Restaurant'){
+						queryString = '\
+							select distinct\
+							  (strafter(str(?r), "#") AS ?property)\
+							where\
+							  { ?r  rdfs:subClassOf :Restaurant}';
+						querys.push(performQuery('Welcher Restaurant', queryString));					  
+					  };					  				  
+				});
+				return Ember.RSVP.allSettled(querys).then(function (data) {
+					console.log("promise all ", data);
+					return data;
+				});
               },
+			  fillModelList: function() {
+				console.log(this.store);
+				console.log("routeElements: "+ document.querySelectorAll(".routeElements"));
+				var routeElements = document.querySelectorAll(".routeElements");
+				nodes = Array.prototype.slice.call(routeElements,0);
+				nodes.forEach((function(r) {
+					if (r.checked) {
+						console.log("checked: " + r.name);
+						var name = r.name;
+						var tempModel = this.store.createRecord('propertyName',{
+							routeName: name
+						});
+					} else{
+						console.log("not checked: " + r.name);
+					}
+				}).bind(this));
+			  },
               actions: {
                 step1: function() {
-				console.log("function: step1")
+				  console.log("function: step1")
                   this.transitionTo('step1');
                 },
                 step3: function() {
+				  this.fillModelList();
                   this.transitionTo('step3');
                 }
               }
@@ -174,7 +226,7 @@
                     where\
                       { ?s ?p ?x  }\
                   ';
-                return performQuery(queryString).then(function(data) {
+                return performQuery(null, queryString).then(function(data) {
                   console.log("Query result ", data);
                   return data.results.bindings;
 
